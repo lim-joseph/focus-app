f08aa39fb3f68377c61574022b468f1c6a8cf31d
Joseph <limjoseph2004@gmail.com>
2024-09-21 00:07:30 +1000

    Add login and signup page

--- focusbear-frontend/app/(tabs)/_layout.tsx
+++ focusbear-frontend/app/(tabs)/_layout.tsx
@@ -1,37 +1,44 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
+import { Tabs } from "expo-router";
+import React from "react";
 
-import { TabBarIcon } from '@/components/navigation/TabBarIcon';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { TabBarIcon } from "@/components/navigation/TabBarIcon";
+import { Colors } from "@/constants/Colors";
+import { useColorScheme } from "@/hooks/useColorScheme";
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
+	const colorScheme = useColorScheme();
 
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
-          ),
-        }}
-      />
-    </Tabs>
-  );
+	return (
+		<Tabs
+			screenOptions={{
+				tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
+				headerShown: false,
+			}}
+		>
+			<Tabs.Screen
+				name="index"
+				options={{
+					title: "Login",
+					tabBarIcon: ({ color, focused }) => (
+						<TabBarIcon
+							name={focused ? "log-in" : "log-in-outline"}
+							color={color}
+						/>
+					),
+				}}
+			/>
+			<Tabs.Screen
+				name="signup"
+				options={{
+					title: "Signup",
+					tabBarIcon: ({ color, focused }) => (
+						<TabBarIcon
+							name={focused ? "person-add" : "person-add-outline"}
+							color={color}
+						/>
+					),
+				}}
+			/>
+		</Tabs>
+	);
 }

62bda9196df6408f4ab4119b692b4f33be1c5b3e
cooi123 <calebooi@hotmail.com>
2024-09-21 03:44:16 +1000

    move signup route to auth

--- focusbear-backend/src/user/dtos/user.dto.ts
+++ focusbear-backend/src/user/dtos/user.dto.ts
@@ -29,3 +29,26 @@ export class UserDto {
     @IsNotEmpty()
     password: string;
 }
+
+export class UserRegisterDto {
+    @ApiProperty({
+        description: 'The name of the user',
+        example: 'John Doe',
+    })
+    @IsString()
+    name: string;
+
+    @ApiProperty({
+        description: 'The email of the user',
+        example: 'johndoe@email.com',
+    })
+    @IsEmail()
+    email: string;
+
+    @ApiProperty({
+        description: 'The password of the user',
+        example: 'password123',
+    })
+    @IsNotEmpty()
+    password: string;
+}
--- focusbear-backend/src/user/user.controller.ts
+++ focusbear-backend/src/user/user.controller.ts
@@ -1,18 +1,11 @@
 import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
-import { UserRegisterDto } from './dtos/userRegister.dto';
 import { UserService } from './user.service';
 
 @ApiTags('Users')
 @Controller('user')
 export class UserController {
     constructor(private userService: UserService) { }
-
-    @Post()
-    createUser(@Body() user: UserRegisterDto) {
-        return this.userService.createUser(user);
-
-    }
 
 
     @Get()

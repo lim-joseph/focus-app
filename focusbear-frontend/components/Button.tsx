032518e03a42f9a5dd78efda20817d4706395594
Toby56 <toby56@pm.me>
2024-09-21 15:42:37 +1000

    Update button with invert property

--- focusbear-frontend/components/Button.tsx
+++ focusbear-frontend/components/Button.tsx
@@ -1,38 +1,59 @@
-import { TouchableOpacity, Text, StyleSheet } from 'react-native';
-import { type ComponentProps } from 'react';
+import { TouchableOpacity, Text, StyleSheet } from "react-native";
+import { type ComponentProps } from "react";
+import { Sizes } from "@/constants/Sizes";
+import { Colors } from "@/constants/Colors";
 
-interface props { title: string | null }
+interface props {
+  title: string | null;
+  inverted: boolean | null;
+}
 
-export function Button({ title, onPress, style }: ComponentProps<typeof TouchableOpacity> & props) {
-	return (
-		<TouchableOpacity
-			onPress={onPress}
-			style={[styles.button, style]}
-		>
-			<Text style={[styles.buttonText]}>{title}</Text>
-		</TouchableOpacity>
-	);
+export function Button({
+  title = "",
+  inverted = false,
+  onPress,
+  style,
+}: ComponentProps<typeof TouchableOpacity> & props) {
+  return (
+    <TouchableOpacity
+      onPress={onPress}
+      style={[styles.button, style, styles[inverted ? "inverted" : "normal"]]}
+    >
+      <Text
+        style={[styles.buttonText, styles[inverted ? "inverted" : "normal"]]}
+      >
+        {title}
+      </Text>
+    </TouchableOpacity>
+  );
 }
 
 const styles = StyleSheet.create({
-	buttonText: {
-		color: "white",
-		fontSize: 16,
-		lineHeight: 21,
-		fontWeight: "bold",
-		letterSpacing: 0.25,
-		fontFamily: "sans-serif"
-	},
+  buttonText: {
+    color: "white",
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: "bold",
+    letterSpacing: 0.25,
+    fontFamily: "sans-serif",
+  },
 
-	button: {
-		backgroundColor: "#e9902c",
-		height: 40,
-		borderRadius: 20,
-		alignItems: "center",
-		justifyContent: "center",
-		paddingVertical: 12,
-		paddingHorizontal: 32,
-		elevation: 3,
-	},
+  button: {
+    height: Sizes.buttonHeight,
+    borderRadius: Sizes.buttonRadius,
+    alignItems: "center",
+    justifyContent: "center",
+    elevation: 3,
+    overflow: "visible",
+    padding: 10,
+  },
+
+  normal: {
+    backgroundColor: Colors.light.tint,
+    color: "white",
+  },
+
+  inverted: {
+    color: Colors.light.tint,
+  },
 });
-
